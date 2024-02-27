c0ea81f5af9b406feaa8561ab78d98527197b56f
gyutae <qoxowkd0716@gmail.com>
2024-02-27 16:19:45 +0900

    Feat: 색상 및 테마 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 import Navbar from "./components/Navbar";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -9,13 +11,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="/create" element={<PostForm />} />
-        <Route path="/posts/:postId" element={<PostDetail />} />
-        <Route path="/posts/edit/:postId" element={<PostEditForm />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PostList />} />
+          <Route path="/create" element={<PostForm />} />
+          <Route path="/posts/:postId" element={<PostDetail />} />
+          <Route path="/posts/edit/:postId" element={<PostEditForm />} />
+        </Routes>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#034694",
+    },
+    secondary: {
+      main: "#FFFFFF",
+      contrastText: "#DAA520",
+    },
+  },
+});
+
+export default theme;
