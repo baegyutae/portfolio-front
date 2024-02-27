90d7756e453c97c47f5717ff53e49259430de2e8
gyutae <qoxowkd0716@gmail.com>
2024-02-27 17:14:56 +0900

    Refactor: 네비게이션바 UI/UX 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,41 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <AppBar position="static" color="primary">
+    <AppBar position="static" color="primary" sx={{ padding: "0.5rem" }}>
       <Toolbar>
-        <Grid container alignItems="center">
-          <Grid item xs={8}>
-            <Typography variant="h6">첼시 팬 움짤 커뮤니티</Typography>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Link
+              to="/"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+                color: "inherit",
+              }}
+            >
+              <Typography variant="h5" component="div">
+                첼시 팬 움짤 커뮤니티
+              </Typography>
+            </Link>
           </Grid>
-          <Grid item xs={2}>
-            <Button color="inherit" component={Link} to="/">
+          <Grid item>
+            <Button
+              color="inherit"
+              variant="outlined"
+              component={Link}
+              to="/"
+              sx={{ marginX: "0.5rem" }}
+            >
               홈
             </Button>
-          </Grid>
-          <Grid item xs={2}>
-            <Button color="inherit" component={Link} to="/create">
+            <Button
+              color="inherit"
+              variant="outlined"
+              component={Link}
+              to="/create"
+              sx={{ marginX: "0.5rem" }}
+            >
               게시글 작성
             </Button>
           </Grid>
--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,15 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#034694",
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+    h1: {
+      fontSize: "2.2rem",
+      fontWeight: 500,
     },
-    secondary: {
-      main: "#FFFFFF",
-      contrastText: "#DAA520",
+    h2: {
+      fontSize: "1.8rem",
+      fontWeight: 500,
     },
   },
 });
