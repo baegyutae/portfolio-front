8a909b53a9630f7253f33895b6a73df8a12f3618
gyutae <qoxowkd0716@gmail.com>
2024-03-13 13:53:18 +0900

    Feat: 로그인 기능 추가

---
+++ src/components/AuthContext.js
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("auth"));
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
+  const login = (userData) => {
+    localStorage.setItem("auth", JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
