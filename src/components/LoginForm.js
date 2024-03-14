e26b5e3d548d4716037ea9632891a6d4cfbb475e
gyutae <qoxowkd0716@gmail.com>
2024-03-14 21:30:48 +0900

    Refactor: 토큰으로 변경

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TextField, Button, Container, Typography, Box } from "@mui/material";
-import { useAuth } from "./AuthContext";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const { login } = useAuth();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -22,16 +20,22 @@ function LoginForm() {
           password,
         }),
       });
-      const data = await response.json();
+
+      const authHeader = response.headers.get("Authorization");
       if (response.ok) {
-        login(data);
-        localStorage.setItem("auth", JSON.stringify(data));
-        navigate("/postlist");
+        // 응답 헤더에서 토큰 추출
+        const token = authHeader.split(" ")[1];
+        // 로컬 스토리지에 토큰 저장
+        localStorage.setItem("token", token);
+        navigate("/postlist"); // 로그인 성공 후 게시글 목록 페이지로 리다이렉션
       } else {
-        throw new Error(data.message || "Login failed");
+        // 에러 메시지가 응답 바디에 있을 경우 응답 바디에서 메시지를 추출
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Login failed");
       }
     } catch (error) {
-      console.error("Login failed:", error);
+      console.error("Login failed:", error.message);
+      alert("Login failed: " + error.message); // 사용자에게 로그인 실패 메시지 표시
     }
   };
 
@@ -39,22 +43,22 @@ function LoginForm() {
     <Container component="main" maxWidth="xs">
       <Box
         sx={{
-          mt: 8,
+          marginTop: 8,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
         <Typography component="h1" variant="h5">
-          로그인
+          Sign in
         </Typography>
-        <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleLogin}>
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
             fullWidth
             id="username"
-            label="사용자 이름"
+            label="Username"
             name="username"
             autoComplete="username"
             autoFocus
@@ -66,7 +70,7 @@ function LoginForm() {
             required
             fullWidth
             name="password"
-            label="비밀번호"
+            label="Password"
             type="password"
             id="password"
             autoComplete="current-password"
@@ -79,15 +83,7 @@ function LoginForm() {
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
           >
-            로그인
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            sx={{ mt: 1, mb: 2 }}
-            onClick={() => navigate("/signup")}
-          >
-            회원가입
+            Sign In
           </Button>
         </Box>
       </Box>
