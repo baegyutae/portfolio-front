45bf3b1021add10c5af771d711a8a62b873d2c2e
gyutae <qoxowkd0716@gmail.com>
2024-03-30 03:37:34 +0900

    Feat: Api 분리

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,16 +1,18 @@
-export const fetchPosts = async () => {
-  const token = localStorage.getItem("token");
+export const submitPost = async (formData, token) => {
   const response = await fetch(
     `${process.env.REACT_APP_API_BASE_URL}/api/posts`,
     {
-      method: "GET",
+      method: "POST",
+      body: formData,
       headers: {
         Authorization: `Bearer ${token}`,
       },
     }
   );
+
   if (!response.ok) {
-    throw new Error("Network response was not ok");
+    throw new Error("게시글 작성 실패");
   }
+
   return response.json();
 };
