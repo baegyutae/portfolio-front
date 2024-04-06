730651107040c09bdea371d160d9b50a57fd9e91
gyutae <qoxowkd0716@gmail.com>
2024-04-07 01:25:54 +0900

    Refactor: 엔드포인트 수정

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -20,7 +20,6 @@ function FileUpload() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // 로컬 스토리지에서 토큰 가져오기
     const token = localStorage.getItem("token");
 
     const formData = new FormData();
@@ -35,28 +34,26 @@ function FileUpload() {
     }
 
     try {
-      // 환경 변수를 사용하여 백엔드 주소 참조
       const response = await fetch(
-        `${process.env.REACT_APP_API_BASE_URL}/api/posts`,
+        `${process.env.REACT_APP_API_BASE_URL}/files/upload`,
         {
           method: "POST",
           body: formData,
           headers: {
-            // 'Authorization' 헤더는 'fetch'의 'headers' 옵션에서 직접 설정할 수 없는 경우가 있으므로, 조건부로 추가
             ...(token ? { Authorization: `Bearer ${token}` } : {}),
           },
         }
       );
 
       if (response.ok) {
-        alert("게시글이 성공적으로 작성되었습니다.");
-        // 성공적으로 게시글이 생성된 후 필요한 동작 (예: 페이지 리디렉션)
+        alert("파일 업로드 및 게시글 작성이 성공적으로 완료되었습니다.");
+        // 성공 후 필요한 동작
       } else {
-        alert("게시글 작성에 실패했습니다.");
+        alert("파일 업로드 및 게시글 작성에 실패했습니다.");
       }
     } catch (error) {
       console.error("Error:", error);
-      alert("게시글 작성 중 오류가 발생했습니다.");
+      alert("파일 업로드 및 게시글 작성 중 오류가 발생했습니다.");
     }
   };
 
